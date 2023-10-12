79e113df08ea9564db311cc70b47d605f25474b9
Lisa <feyleiiia@gmail.com>
2023-10-12 19:51:31 +0300

    first commit

--- src/main/java/sorts/QuickSort.java
+++ src/main/java/sorts/QuickSort.java
@@ -1,4 +1,67 @@
 package sorts;
 
+import java.util.Arrays;
+
 public class QuickSort {
+    public static void main(String[] args) {
+        int[] array = {64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
+
+        System.out.println(printArray(array));
+
+        quickSort(array, 0, array.length - 1);
+
+        System.out.println(printArray(array));
+
+    }
+
+    private static void quickSort(int[] arr, int from, int to){
+        if (from < to){
+            int divideIndex = partition(arr, from, to);
+
+            quickSort(arr, from, divideIndex - 1);
+
+            quickSort(arr, divideIndex, to);
+        }
+
+    }
+
+    private static int partition(int[] arr, int from, int to){
+        int leftIndex = from;
+        int rightIndex = to;
+
+        int pivot = arr[from + (to - from) / 2];
+
+        while (leftIndex <= rightIndex) {
+            while (arr[leftIndex] < pivot) {
+                leftIndex++;
+            }
+            while (arr[rightIndex] > pivot) {
+                rightIndex--;
+            }
+            if (leftIndex <= rightIndex) {
+                swap(arr, leftIndex, rightIndex);
+                leftIndex++;
+                rightIndex--;
+            }
+        }
+        return leftIndex;
+    }
+
+    private static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+    private static String printArray(int[] arr){
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < arr.length; i++){
+            if (i > 0){
+                sb.append(", ");
+            }
+            sb.append(arr[i]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
